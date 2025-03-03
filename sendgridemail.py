eb5fb91b459b00dd40511bf3223fa37542656f75
Matt Rkiouak <mrkiouak@gmail.com>
2025-03-02 17:30:39 -0800

    WiP sendgrid

---
+++ sendgridemail.py
@@ -0,0 +1,27 @@
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
+from google.cloud import firestore
+from google.cloud.firestore_v1 import FieldFilter
+import secretmanager
+
+def get_to_emails():
+    db = firestore.Client()
+    users_ref = db.collection("users")
+    queried_users = users_ref.where(filter=FieldFilter("username", "==", username)).get()
+    return [user.email for user in queried_users.to_dict()]
+
+message = Mail(
+    from_email='matt@rkiouak.com',
+    to_emails='mrkiouak@gmail.com',
+    subject='Hello from SendGrid',
+    html_content='<strong>Hello, Email!</strong>'
+)
+try:
+    sg = SendGridAPIClient(os.environ.get(secretmanager.get_secret("projects/4042672389/secrets/sendgrid-api-key/versions/latest")))
+    response = sg.send(message)
+    print(f"Status Code: {response.status_code}")
+    print(f"Body: {response.body}")
+    print(f"Headers: {response.headers}")
+except Exception as e:
+    print(f"Error: {e}")

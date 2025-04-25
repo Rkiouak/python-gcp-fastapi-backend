b24f284c7c83242bcd9b7ad45c4426d185d5cef9
Matt Rkiouak <mrkiouak@gmail.com>
2025-04-24 22:23:08 -0700

    clean up email verification during sign up

--- sendgridemail.py
+++ sendgridemail.py
@@ -14,11 +14,12 @@ def get_to_emails():
     return [user.email for user in queried_users.to_dict()]
 
 def send_email(user: SignUpUser, challenge: str):
+    name = " "+user.given_name if user.given_name is not None else ""
     message = Mail(
         from_email='matt@rkiouak.com',
         to_emails=user.email,
         subject='Complete your musings-mr.net sign up',
-        html_content=f'<strong>Hello, {user.given_name},</strong>\n To complete your musings-mr.net signup, please visit https://musings-mr.net/users/validate?challenge={challenge}'
+        html_content=f'<strong>Hello{name},</strong><p>To complete your musings-mr.net signup, please click <a href=\"https://musings-mr.net/users/validate?challenge={challenge}\"> here</a> </p>'
     )
     try:
         sg = sendgrid.SendGridAPIClient(secretmanager.get_secret("projects/4042672389/secrets/sendgrid-api-key/versions/latest"))
